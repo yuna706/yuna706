7aa2c0d21e6969ab18572bb1debe32d241b0b446
yuna706 <riyu6027@naver.com>
2020-07-19 22:22:58 +0900

    스크롤 이벤트 제거

--- js.js
+++ js.js
@@ -28,6 +28,8 @@ $(function() {
         audio.currentTime = 0;
     });
 
+
+    
     $(".side> .menu> li").on("click",function(e){
         e.preventDefault();
         $("#sidemenu").css("margin-left","-390px");
@@ -48,7 +50,8 @@ $(function() {
 
         $("html, body").stop().animate({"scrollTop":top}, 1000, "easeOutQuad");
     });
-
+    
+ /*
     let scrolling = false;
 
     $("body> section").on("wheel", function(e){
@@ -69,6 +72,8 @@ $(function() {
             $("html, body").stop().animate({"scrollTop": prev}, 1500, "easeOutQuad", function(){scrolling = 0;});
         }
     });
+*/
+
 
     /*
     $(window).scroll(function(){
