368204fce432622346a6b0c5885f289df832b824
yuna706 <riyu6027@naver.com>
2023-05-11 20:03:21 +0900

    레이아웃 변경

--- js.js
+++ js.js
@@ -51,7 +51,7 @@ $(function() {
         $("html, body").stop().animate({ "scrollTop": top }, 1000, "easeOutQuad");
     });
 
-    $("nav> ul> li").on("click", function(e){
+    $("nav> ul> li, .footer-div> .menu> li").on("click", function(e){
         e.preventDefault();
 
         let id = $(e.currentTarget).data("value");
@@ -89,78 +89,5 @@ $(function() {
         $("#popup").removeClass("on");
         $(".popup").css({"visibility":"hidden"});
         $(".ham").css("display", "block");
-    });
-
-    // hobby에서 사진 갤러리
-    $(".tab-div> div").on("click", function(){
-        let b = $(this).index();
-
-        $(".cont").removeClass("on");
-        $(".tab-div> div").removeClass("on");
-        $(".cont").eq(b).addClass("on");
-        $(".tab-div> div").eq(b).addClass("on");
-    });
-
-    //jpg 파일 확장자 -> gif로 변환
-    $("#ppt> ul> li").click(function(){
-        let hwac = $(this).children("img").attr("src").split(".");
-        let hwac2 = $("#ppt .cont2> img").attr("src").split(".");
-        let name = "";
-
-        name = hwac[0] + ".gif";
-        $(this).children("img").attr("src", name);
-
-        name = hwac2[0] + ".png";
-        $("#ppt .cont2> img").attr("src", name);
-
-        name ="";
-    })
-
-    //중앙으로 배치
-    $(".cont> ul> li").on("click",function(){
-        let a = $(this).attr("class");
-        if(a == "cont1"){
-            $(this).removeClass("cont1").addClass("cont2").siblings(".cont2").removeClass("cont2").addClass("cont1");
-        }
-        else if(a == "cont3"){
-            $(this).removeClass("cont3").addClass("cont2").siblings(".cont2").removeClass("cont2").addClass("cont3");
-        }
-        else if(a == "cont2"){
-            alert("다시 움직이게 하려면 양 옆의 사진을 눌러주세요 :)");
-        }
-
-    });
-
-    //colors
-    function bg_on(color){
-        let re = new Array;
-
-        if(color == "violet"){
-            re[0] = ".vil";
-            re[1] = ".violet-b";
-        }
-        else if(color == "gray"){
-            re[0] = ".gr";
-            re[1] = ".gray-b";
-        }
-        else if(color == "blue"){
-            re[0] = ".br";
-            re[1] = ".blue-b";
-        }
-        else if(color == "green"){
-            re[0] = ".gn";
-            re[1] = ".green-b";
-        }
-
-        return re;
-    }
-
-    $(".footer-div> .menu> div").on("click",function(e){
-        e.preventDefault();
-
-        let idx = $(this).index()+1;
-        let top = idx * he;
-
-        $("html, body").stop().animate({"scrollTop":top}, 1000, "easeOutQuad");
     });
 });
